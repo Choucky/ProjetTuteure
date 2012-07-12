1b185aa477a1a38539a62a9499d4a5c9399b5bc4
elodie <elodie@elodie-SF311>
2012-07-12 21:15:22 +0200

    Information terminé + test OK Design 80%terminé

--- Generateur/IYWSLibrary/IYWSDesign.php
+++ Generateur/IYWSLibrary/IYWSDesign.php
@@ -19,14 +19,25 @@ class IYWSDesign
 	 * \details	Récupération des informations stockées dans la base de données correspondant à la table DESIGN.
 	 */
 	public function IYWSUser($code){
+		parent::IYWSDatabase();
 		try {
+			//Récupération des informations dans la base de données
 			$query = $this->db->query("SELECT * FROM USER WHERE code=".$code);
 			$query->setFetchMode(PDO::FETCH_OBJ);
 			$array = $query->fetch();
 
-			$this->code = $array->code;
-			$this->name = $array->name;
-			$this->image = $array->image;
+			//Si les infos existe alors on affecte les attributs sinon on retourne une erreur
+			if( $array != false ){
+				$this->code = $array->code;
+				$this->name = $array->name;
+				$this->image = $array->image;
+				
+				$this->error = IYWS_OK;
+			}
+			else{
+				$this->error = IYWS_ERR_NOTEXISTS;
+			}
+			
 		} catch (Exception $e) {
 			$this->error = IYWS_ERR_DB;
 		}
@@ -58,8 +69,9 @@ class IYWSDesign
 	
 	/**
 	 * \brief 	Setter du code
-	 * \return	Void
+	 * \return	\e IYWSDesign
 	 * \details	Met à jour le code du site.
+	 * \param 	\e $code Structure du code pour le design.
 	 */
 	public function setCode($code){
 		$this->code = $code;
@@ -68,8 +80,9 @@ class IYWSDesign
 	
 	/**
 	 * \brief 	Setter de name
-	 * \return	Void
+	 * \return	\e IYWSDesign
 	 * \details	Met à jour le nom du site.
+	 * \param	\e $name Nom du design du site.
 	 */
 	public function setName($name){
 		$this->name = $name;
@@ -78,11 +91,43 @@ class IYWSDesign
 	
 	/**
 	 * \brief	Setter de Image
-	 * \return	Void
+	 * \return	\e IYWSDesign
 	 * \details	Met à jour le lien de l'image du site.
+	 * \param	\e $image lien de l'image pour le design du site web.
 	 */
 	public function setImage($image){
 		$this->image = $image;
 		return $this;
+	}
+	
+	/**
+	 * \brief	Storage
+	 * \details	Ajout des informations dans la base de données.
+	 * \return	true si la requête est exécuté sinon false
+	 */
+	public function store(){
+		try {
+			if( $this->id != NULL )
+			{
+				//On met à jour la base de données
+				$query = $this->db->exec("UPDATE DESIGN SET name=" . $this->db->quote($this->name) . ","
+						. "image=" . $this->db->quote($this->image) . ","
+						. "code=" . $this->db->quote($this->code)
+						. "WHERE id_design=" . $this->db->quote($this->id)
+				);
+	
+				var_dump($query);
+				$this->error = IYWS_OK;
+				return true;
+			}
+			else
+			{
+				$this->error = IYWS_ERR_NOTEXISTS;
+				return false;
+			}
+		} catch (Exception $e) {
+			$this->error = IYWS_ERR_DB;
+			return false;
+		}
 	}
 }
